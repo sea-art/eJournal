eed2c1d640a95ecf07556fd3d2079d716ff9910a
Zi Long Zhu <zilong.zhu31@gmail.com>
2018-06-07 16:48:02 +0200

    comments

--- src/main/django/VLE/models.py
+++ src/main/django/VLE/models.py
@@ -1,18 +1,30 @@
+# Database file
 from django.db import models
 
 
 class User(models.Model):
-    ADMIN = 'AD'
+    """
+    User is an entity in the database with the following features:
+    - group: the group determines the permissions on the application.
+    - email: email of the user.
+    - username: username of the username.
+    - passhash: the hash of the password of the user.
+    - education: the education institute of the userself.
+    - lti_id: the DLO id of the user.
+    """
+    SUPERUSER = 'SU'
+    EXAMINATOR = 'EX'
     TEACHER = 'TE'
     TEACHER_ASSISTANT = 'TA'
     STUDENT = 'SD'
     USER_TYPES = (
-        (ADMIN, 'Admin'),
+        (SUPERUSER, 'Super User'),
+        (EXAMINATOR, 'Examintor'),
         (TEACHER, 'Teacher'),
-        (TEACHER_ASSISTANT, 'Teacher assistant'),
+        (TEACHER_ASSISTANT, 'Teacher Assistant'),
         (STUDENT, 'Student'),
     )
-    type = models.TextField(
+    group = models.TextField(
         max_length=2,
         choices=USER_TYPES,
         default=STUDENT,
@@ -38,20 +50,42 @@ class User(models.Model):
         return self.username
 
     class Meta:
+        """
+        A class for meta data.
+        - unique_together: username and education must be unique together.
+        """
         unique_together = ('username', 'education',)
 
 
 class Course(models.Model):
+    """
+    A Course entity has the following features:
+    - name: name of the course.
+    - author: the creator of the course.
+    - abbrevation: a max three letter abbrevation of the course name.
+    - startdate: the date that the course starts.
+    """
     name = models.TextField()
     author = models.ManyToManyField(User)
-    abbreviation = models.TextField(max_length=3)
-    startdate = models.DateField()
+    abbreviation = models.TextField(
+        max_length=3,
+        default='XXX',
+    )
+    startdate = models.DateField(
+        null=True,
+    )
 
     def __str__(self):
         return self.name
 
 
 class Assignment(models.Model):
+    """
+    An Assignment entity has the following features:
+    - name: name of the assignment.
+    - description: description for the assignment.
+    - course: a foreign key linked to a course.
+    """
     name = models.TextField()
     description = models.TextField(
         null=True,
@@ -63,6 +97,11 @@ class Assignment(models.Model):
 
 
 class Journal(models.Model):
+    """
+    A journal contains the following features:
+    - assignment: a foreign key linked to an assignment.
+    - user: a foreign key linked to an user.
+    """
     assignment = models.ForeignKey(
         'Assignment',
         on_delete=models.CASCADE
@@ -77,14 +116,20 @@ class Journal(models.Model):
 
 
 class Entry(models.Model):
+    """
+    An Entry has the following features:
+    - journal: a foreign key linked to an Journal.
+    - datetime: the date and time when the entry was posted.
+    - late: if the entry was posted late or not.
+    """
     journal = models.ForeignKey(
         'Journal',
         on_delete=models.CASCADE
     )
-    datatime = models.DateTimeField(
+    datetime = models.DateTimeField(
         auto_now_add=True
     )
     late = models.BooleanField()
 
     def __str__(self):
-        return "Entry: " + str(self.pk)
+        return str(self.pk)
