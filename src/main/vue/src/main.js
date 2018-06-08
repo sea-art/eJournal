7c7241257ce18d0c90659dd10c1b2373f93632de
Okke van Eck <okke.van.eck@gmail.com>
2018-06-08 13:04:59 +0200

    Added bootstrap and vue combo

--- src/main/vue/src/main.js
+++ src/main/vue/src/main.js
@@ -4,12 +4,11 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import BootstrapVue from 'bootstrap-vue'
-import '../node_modules/bootstrap/dist/bootstrap.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
 import '../node_modules/bootstrap-vue/dist/bootstrap-vue.css'
 
-
 Vue.config.productionTip = false
-Vue.user(BootstrapVue)
+Vue.use(BootstrapVue)
 
 /* eslint-disable no-new */
 new Vue({
