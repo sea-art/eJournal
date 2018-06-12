8f9ca78c249d5084a40fd47b452209ead40e85a4
Joey Lai <joeylai96@hotmail.com>
2018-06-12 13:42:35 +0200

    Added error page for wrong paths

---
+++ src/vue/src/views/ErrorPage.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>404</h1>
+        <img src="../assets/ohno.jpeg">
+        <router-link tag="button" :to='"/"'>Back to landing page</router-link>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Error'
+}
+</script>
+
+<style>
+</style>
