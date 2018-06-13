5b118415e2ae4cacd82bae0d4a4d7731d31f3156
Rick <rickw320@hotmail.com>
2018-06-13 16:31:05 +0200

    Refresh, needs testing

--- src/vue/src/api/auth.js
+++ src/vue/src/api/auth.js
@@ -13,8 +13,17 @@ export default {
                 localStorage.setItem('jwt_access', response.data.access)
                 localStorage.setItem('jwt_refresh', response.data.refresh)
             })
-            .catch(error => {
-                throw error
+    },
+
+    /* Refresh the access token.
+     * Requests the api server for a new JWT token, given the refresh token.
+     * Stores this new token in jwt_access.
+     * Returns a new Promise that can be used to chain more requests.
+     */
+    refresh () {
+        return connection.conn.post('token/refresh/', {refresh: localStorage.getItem('jwt_refresh')})
+            .then(response => {
+                localStorage.setItem('jwt_access', response.data.access)
             })
     },
 
@@ -35,10 +44,30 @@ export default {
     authenticated_post (url, data, options = {}) {
         connection.conn.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('jwt_access')
         return connection.conn.post(url, data, options)
+            .catch(error => {
+                if(error.data.code === "token_not_valid")
+                {
+                    return refresh().then(connection.conn.post(url, data, options))
+                }
+                else
+                {
+                    throw error
+                }
+            })
     },
 
     authenticated_get (url, options = {}) {
         connection.conn.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('jwt_access')
         return connection.conn.get(url, options)
-    }
+            .catch(error => {
+                if(error.data.code === "token_not_valid")
+                {
+                    return refresh().then(connection.conn.post(url, data, options))
+                }
+                else
+                {
+                    throw error
+                }
+            })
+    },
 }
