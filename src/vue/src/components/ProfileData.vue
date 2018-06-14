87be67ae86d276c922f527700b9d505dc75c0390
Joey Lai <joeylai96@hotmail.com>
2018-06-14 12:01:50 +0200

    Added basic profile components and view

---
+++ src/vue/src/components/ProfileData.vue
@@ -0,0 +1,20 @@
+<template>
+    <b-row>
+        <b-col cols="2">
+            <!-- <img src={{ image }}> -->
+            <img src='../assets/ohno.jpeg'>
+        </b-col>
+        <b-col cols="5">
+            <h7>{{ name }}</h7><br>
+            <p class="profile">{{ email }}</p>
+            <p class="profile">{{ id }}</p>
+        </b-col>
+        <b-col cols="5">TOP RIGHT</b-col>
+    </b-row>
+</template>
+
+<script>
+export default {
+    props: ['name', 'email', 'id', 'image']
+}
+</script>
