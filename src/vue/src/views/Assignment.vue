1570f99343634d83b04ea684d908a0c5b9cc5bf8
Engel Hamer <engelhamer@gmail.com>
2018-06-15 00:02:18 +0200

    Hardcoded 'Rick' as student name in link to journal page in order to avoid false relog in prompt.

--- src/vue/src/views/Assignment.vue
+++ src/vue/src/views/Assignment.vue
@@ -6,13 +6,17 @@
     <content-columns>
         <bread-crumb :currentPage="$route.params.assignmentName" :course="$route.params.courseName" slot="main-content-column"></bread-crumb>
         <div v-for="journal in assignmentJournals" :key="journal.uid" slot="main-content-column">
-            <b-link tag="b-button" :to="{name: 'Journal', params: {student: journal.studentname,
-                                                          course: $route.params.course,
-                                                          assign: $route.params.assign,
-                                                          journal: journal.uid,
-                                                          courseName: $route.params.courseName,
-                                                          assignmentName: $route.params.assignmentName,
-                                                          journalName: journal.student}}">
+            <b-link tag="b-button" :to="{ name: 'Journal',
+                                          params: {
+                                              course: $route.params.course,
+                                              assign: $route.params.assign,
+                                              student: 'Rick',
+                                              color: $route.params.color,
+                                              courseName: $route.params.courseName,
+                                              assignmentName: $route.params.assignmentName,
+                                              journalName: journal.student
+                                          }
+                                        }">
                 <student-card
                     :student="journal.student"
                     :studentNumber="journal.studentNumber"
