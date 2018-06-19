5b0daa5452e874df0a7123fa8e392f4a1f95acb2
Maarten van Keulen <mjvkeulen1@gmail.com>
2018-06-19 11:57:19 +0200

    nearly there.

--- src/vue/src/components/StudentCard.vue
+++ src/vue/src/components/StudentCard.vue
@@ -1,4 +1,29 @@
 <template>
+    <b-card :class="color">
+        <b-row>
+            <b-col order="1" cols="5">
+                <!-- TODO switch src back to :src="portraitPath", square images only -->
+                <img class="img-fluid" src="../assets/logo.png">
+            </b-col>
+            <b-col order="2" cols="7" order-lg="3" lg="2">
+                <todo-square :num="this.stats.submitted - this.stats.graded"/>
+            </b-col>
+            <b-col order="3" cols="12" order-lg="2" lg="5">
+                {{ student}}
+            </b-col>
+        </b-row>
+            <b-col cols="12">
+                {{ studentNumber }}
+            </b-col>
+            <b-col cols="12">
+                <progress-bar :currentPoints="this.stats.acquired_points" :totalPoints="this.stats.total_points"/>
+            </b-col>
+        <b-row>
+        </b-row>
+    </b-card>
+</template>
+
+<!-- <template>
     <b-card class="card container-fluid" :class="color">
         <b-row>
             <b-col cols="3">
@@ -19,7 +44,7 @@
             {{ studentNumber }}: {{ student }}
         </b-row>
     </b-card>
-</template>
+</template> -->
 
 <script>
 import progressBar from '@/components/ProgressBar.vue'
