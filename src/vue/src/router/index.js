0fed97e2a31abf2948c44835720c19e91f0679d3
Okke van Eck <okke.van.eck@gmail.com>
2018-06-19 10:11:07 +0200

    Merge with develop

--- src/vue/src/router/index.js
+++ src/vue/src/router/index.js
@@ -10,7 +10,6 @@ import Register from '@/views/Register'
 import LtiLaunch from '@/views/LtiLaunch'
 import CourseCreation from '@/views/CourseCreation'
 import AssignmentsOverview from '@/views/AssignmentsOverview'
-import Error404 from '@/views/Errors/404.vue'
 
 Vue.use(Router)
 
@@ -58,9 +57,5 @@ export default new Router({
         path: '/lti/launch',
         name: 'LtiLaunch',
         component: LtiLaunch
-    }, {
-        path: '/404',
-        name: 'Error404',
-        component: Error404
     }]
 })
