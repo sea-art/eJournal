5c4ae10db7cf977ffc30a63a5e813507c7fe1817
Xavier van Dommelen <pacoxavierpaco@hotmail.nl>
2018-06-21 14:00:59 +0200

    reworked the journal page for splitting the student and teacher later on

--- src/vue/src/views/Journal.vue
+++ src/vue/src/views/Journal.vue
@@ -1,48 +1,11 @@
 <template>
-    <b-row no-gutters>
-        <!-- TODO: reopen bread-crumb when it is working again -->
-        <b-col v-if="bootstrapLg()" cols="12">
-            <!-- <bread-crumb v-if="bootstrapLg()" @eye-click="customisePage" :currentPage="$route.params.assignmentName" :course="$route.params.courseName"/> -->
-            <edag @select-node="selectNode" :selected="currentNode" :nodes="nodes"/>
-        </b-col>
-        <b-col v-else xl="3" class="left-content">
-            <edag @select-node="selectNode" :selected="currentNode" :nodes="nodes"/>
-        </b-col>
-
-        <b-col lg="12" xl="6" order="2" class="main-content">
-            <!-- <bread-crumb v-if="!bootstrapLg()" @eye-click="customisePage" :currentPage="$route.params.assignmentName" :course="$route.params.courseName"/> -->
-            <div v-if="nodes.length > currentNode">
-                <div v-if="nodes[currentNode].type == 'e'">
-                    <entry-node ref="entry-template-card" @edit-node="adaptData" :entryNode="nodes[currentNode]"/>
-                </div>
-                <div v-else-if="nodes[currentNode].type == 'd'">
-                    {{nodes[currentNode]}}
-                    <entry-node ref="entry-template-card" @edit-node="adaptData" :entryNode="nodes[currentNode]"/>
-                </div>
-                <div v-else-if="nodes[currentNode].type == 'a'">
-                    <add-card @info-entry="addNode" :addNode="nodes[currentNode]"></add-card>
-                </div>
-                <div v-else-if="nodes[currentNode].type == 'p'">
-                    <b-card class="card main-card noHoverCard" :class="'pink-border'">
-                        <h2>Needed progress</h2>
-                        You have {{progressNodes[nodes[currentNode].nID]}} points out of the {{nodes[currentNode].target}}
-                        needed points before {{nodes[currentNode].deadline}}.
-                    </b-card>
-                </div>
-            </div>
-        </b-col>
-        <b-col cols="12" xl="3" order="3" class="right-content"/>
-    </b-row>
+    <journal-student :cID="cID" :aID="aID" :jID="jID"/>
 </template>
 
 <script>
 import contentColumns from '@/components/ContentColumns.vue'
-import entryTemplate from '@/components/TemplateCard.vue'
-import entryNode from '@/components/EntryNode.vue'
-import addCard from '@/components/AddCard.vue'
-import edag from '@/components/Edag.vue'
+import journalStudent from '@/components/JournalStudent.vue'
 import breadCrumb from '@/components/BreadCrumb.vue'
-import journal from '@/api/journal'
 
 export default {
     name: 'Journal',
@@ -51,69 +14,10 @@ export default {
 
     data () {
         return {
-            windowWidth: 0,
-            currentNode: 0,
-            editedData: ['', ''],
-            nodes: [],
-            progressNodes: {}
+            windowWidth: 0
         }
     },
-
-    created () {
-        journal.get_nodes(this.jID)
-            .then(response => { this.nodes = response.nodes })
-            .catch(_ => alert('Error while loading nodes.'))
-    },
-
     methods: {
-        adaptData (editedData) {
-            this.nodes[this.currentNode] = editedData
-        },
-        selectNode ($event) {
-            if ($event === this.currentNode) {
-                return this.currentNode
-            }
-
-            if (this.nodes[this.currentNode].type !== 'e' || this.nodes[this.currentNode].type !== 'd') {
-                this.currentNode = $event
-                return
-            }
-
-            if (this.$refs['entry-template-card'].saveEditMode === 'Save') {
-                if (!confirm('Oh no! Progress will not be saved if you leave. Do you wish to continue?')) {
-                    return
-                }
-            }
-
-            if (this.nodes[$event].type === 'p') {
-                this.progressPoints(this.nodes[$event])
-            }
-
-            this.$refs['entry-template-card'].cancel()
-            this.currentNode = $event
-        },
-        addNode (infoEntry) {
-            // console.log(infoEntry)
-            journal.create_entry(this.jID, infoEntry[0].tID, infoEntry[1])
-            journal.get_nodes(this.jID)
-                .then(response => { this.nodes = response.nodes })
-                .catch(_ => alert('Error while loading nodes.'))
-        },
-        progressPoints (progressNode) {
-            var tempProgress = 0
-
-            for (var node of this.nodes) {
-                if (node.nID === progressNode.nID) {
-                    break
-                }
-
-                if (node.type === 'e' || node.type === 'd') {
-                    tempProgress += node.entry.grade
-                }
-            }
-
-            this.progressNodes[progressNode.nID] = tempProgress
-        },
         getWindowWidth (event) {
             this.windowWidth = document.documentElement.clientWidth
         },
@@ -145,10 +49,7 @@ export default {
     components: {
         'content-columns': contentColumns,
         'bread-crumb': breadCrumb,
-        'entry-template': entryTemplate,
-        'add-card': addCard,
-        'edag': edag,
-        'entry-node': entryNode
+        'journal-student': journalStudent
     }
 }
 </script>
