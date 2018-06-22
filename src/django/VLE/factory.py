313118de28deab5a9e488cd04346f739ba1142d5
Lars der Polo <lars.vanhijfte@student.uva.nl>
2018-06-22 14:31:11 +0200

    Done with flake for now

--- src/django/VLE/factory.py
+++ src/django/VLE/factory.py
@@ -1,10 +1,18 @@
-from VLE.models import *
+"""
+factory.py.
+
+The facory has all kinds of functions to create entries in the database.
+Sometimes this also supports extra functionallity like adding courses to assignments.
+"""
+from VLE.models import User, Participation, Course, Assignment, Role, JournalFormat, PresetNode, Node, EntryComment, \
+    Entry, EntryTemplate, Field, Content, Deadline, Journal
 import random
 import datetime
 import django.utils.timezone as timezone
 
 
 def make_user(username, password, email=None, lti_id=None, profile_picture=None, is_admin=False):
+    """Create a user."""
     user = User(username=username, email=email, lti_id=lti_id, is_admin=is_admin)
     user.save()
     user.set_password(password)
@@ -16,13 +24,15 @@ def make_user(username, password, email=None, lti_id=None, profile_picture=None,
     return user
 
 
-def make_participation(user, course, role):
+def make_participation(user=None, course=None, role=None):
+    """Create a participation."""
     participation = Participation(user=user, course=course, role=role)
     participation.save()
     return participation
 
 
 def make_course(name, abbrev, startdate=None, author=None, lti_id=None):
+    """Create a course."""
     course = Course(name=name, abbreviation=abbrev, startdate=startdate, author=author, lti_id=lti_id)
     course.save()
     if author:
@@ -35,7 +45,7 @@ def make_course(name, abbrev, startdate=None, author=None, lti_id=None):
 
 
 def make_assignment(name, description, author=None, format=None, cIDs=None, courses=None):
-    """Makes a new assignment
+    """Make a new assignment.
 
     Arguments:
     name -- name of assignment
@@ -62,6 +72,14 @@ def make_assignment(name, description, author=None, format=None, cIDs=None, cour
 
 
 def make_format(templates=[], max_points=10):
+    """Make a format.
+
+    Arguments:
+    templates -- list of all the templates to add to the format.
+    max-points -- maximum points of the format (default: 10)
+
+    Returns the format
+    """
     format = JournalFormat(max_points=max_points)
     format.save()
     format.available_templates.add(*templates)
@@ -69,12 +87,25 @@ def make_format(templates=[], max_points=10):
 
 
 def make_progress_node(format, deadline):
+    """Make a progress node.
+
+    Arguments:
+    format -- format the node belongs to.
+    deadline -- deadline of the node.
+    """
     node = PresetNode(type=Node.PROGRESS, deadline=deadline, format=format)
     node.save()
     return node
 
 
 def make_entrydeadline_node(format, deadline, template):
+    """Make entry deadline.
+
+    Arguments:
+    format -- format of the entry deadline.
+    deadline -- deadline en the entry deadline.
+    template -- template of the entrydeadline.
+    """
     node = PresetNode(type=Node.ENTRYDEADLINE, deadline=deadline,
                       forced_template=template, format=format)
     node.save()
@@ -82,14 +113,20 @@ def make_entrydeadline_node(format, deadline, template):
 
 
 def make_node(journal, entry):
+    """Make a node.
+
+    Arguments:
+    journal -- journal the node belongs to.
+    entry -- entry the node belongs to.
+    """
     node = Node(type=Node.ENTRY, entry=entry, journal=journal)
     node.save()
     return node
 
 
 def make_journal(assignment, user):
-    """
-    Creates a new journal.
+    """Make a new journal.
+
     First creates all nodes defined by the format.
     The deadlines and templates are the same object
     as those in the format, so any changes should
@@ -107,8 +144,8 @@ def make_journal(assignment, user):
 
 
 def make_entry(template, posttime=timezone.now()):
-    """
-    Creates a new entry in a journal.
+    """Create a new entry in a journal.
+
     Posts it at the specified moment, or when unset, now.
     -journal is the journal to post the entry in.
     -posttime is the time of posting, defaults to current time.
@@ -121,24 +158,33 @@ def make_entry(template, posttime=timezone.now()):
 
 
 def make_entry_template(name):
+    """Make an entry template."""
     entry_template = EntryTemplate(name=name)
     entry_template.save()
     return entry_template
 
 
 def make_field(template, descrip, loc, type=Field.TEXT):
+    """Make a field."""
     field = Field(type=type, title=descrip, location=loc, template=template)
     field.save()
     return field
 
 
 def make_content(entry, data, field=None):
+    """Make content."""
     content = Content(field=field, entry=entry, data=data)
     content.save()
     return content
 
 
 def make_deadline(datetime=datetime.datetime.now(), points=None):
+    """Make dealdine/points.
+
+    Arguments:
+    datetime -- time the deadline ends (default: now)
+    points -- points that have to be aquired to pass the deadline.
+    """
     if points:
         deadline = Deadline(datetime=datetime, points=points)
     else:
@@ -148,6 +194,7 @@ def make_deadline(datetime=datetime.datetime.now(), points=None):
 
 
 def make_journal_format():
+    """"Make a journal format."""
     journal_format = JournalFormat()
     journal_format.save()
     return journal_format
@@ -156,6 +203,7 @@ def make_journal_format():
 def make_role(name, can_edit_grades=False, can_view_grades=False, can_edit_assignment=False,
               can_view_assignment=False, can_submit_assignment=False, can_edit_course=False,
               can_delete_course=False):
+    """Make a role."""
     role = Role(
         name=name,
         can_edit_grades=can_edit_grades,
@@ -171,7 +219,8 @@ def make_role(name, can_edit_grades=False, can_view_grades=False, can_edit_assig
 
 
 def make_entrycomment(entryID, author, text):
-    """
+    """Make an Entry Comment.
+
     Make an Entry Comment for an entry based on its ID.
     With the author and the given text.
     """
