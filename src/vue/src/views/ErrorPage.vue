118e9ca9ada160348c3abec3aeb47299cc80cc2d
Engel Hamer <engelhamer@gmail.com>
2018-06-25 15:12:40 +0200

    Updated error page.

--- src/vue/src/views/ErrorPage.vue
+++ src/vue/src/views/ErrorPage.vue
@@ -1,26 +1,51 @@
 <template>
-    <content-columns>
-        <h1 slot="main-content-column">{{ errorCode }}' : '{{ errorMessage }}</h1>
-        <img src="/static/oh_no/ohno.jpeg" slot="main-content-column">
-        <router-link tag="button" :to='"/"' slot="main-content-column">Back to landing page</router-link>
-    </content-columns>
+    <b-row class="error-content">
+        <b-col cols="12">
+            <h1 id="error-heading">Error {{ errorCode }}:</h1><h1 id="error-message"> {{ errorMessage }}</h1><br/>
+        </b-col>
+        <b-col cols="12">
+            Unfortunately the page you are looking for is not here.<br/>
+            You might want to try going back to the homepage or report a problem.
+        </b-col>
+        <b-col cols="12">
+            <b-button :to='"/Home"'>Home</b-button>
+            <b-button :to='"/Home"'>Contact</b-button>
+        </b-col>
+    </b-row>
 </template>
 
 <script>
-import contentColumns from '@/components/ContentColumns.vue'
-
 export default {
     name: 'Error',
     props: {
-        errorMessage: {
-            required: true
-        },
         errorCode: {
             required: true
+        },
+        errorMessage: {
+            required: true
         }
-    },
-    components: {
-        'content-columns': contentColumns
     }
 }
 </script>
+
+<style>
+.error-content {
+    padding: 40px;
+}
+
+#error-heading, #error-message {
+    font-size: 100px;
+    border: none;
+    padding: 0px;
+}
+
+#error-heading {
+    color: var(--theme-dark-grey);
+    font-size: 80px;
+}
+
+#error-message {
+    color: var(--theme-medium-grey);
+    font-size: 60px;
+}
+</style>
