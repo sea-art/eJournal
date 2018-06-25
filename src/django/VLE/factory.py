46c2ec90defa43f8c801c837f2b4c34bffe6ef12
Okke van Eck <okke.van.eck@gmail.com>
2018-06-25 15:04:50 +0200

    Removed print statement

--- src/django/VLE/factory.py
+++ src/django/VLE/factory.py
@@ -92,9 +92,6 @@ def make_assignment(name, description, author=None, format=None, lti_id=None,
     if courses:
         for course in courses:
             assign.courses.add(course)
-
-    print(lti_id)
-
     if lti_id is not None:
         assign.lti_id = lti_id
     if points_possible is not None:
