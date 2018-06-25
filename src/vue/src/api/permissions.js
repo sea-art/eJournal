2ea0ab9414c4c3b3392e8640541406834a1373f5
Zi Long Zhu <zilong.zhu31@gmail.com>
2018-06-25 15:38:41 +0200

    get & update api for role permissions  front end

--- src/vue/src/api/permissions.js
+++ src/vue/src/api/permissions.js
@@ -5,5 +5,18 @@ export default {
     get_course_permissions (cID) {
         return auth.authenticatedGet('/get_course_permissions/' + cID + '/')
             .then(response => response.data.permissions)
+    },
+
+    get_course_roles (cID) {
+        return auth.authenticatedGet('/get_course_roles/' + cID + '/')
+            .then(response => response.data.roles)
+    },
+
+    update_course_roles (cID, roles) {
+        return auth.authenticatedPost('/update_course_roles/', {
+            cID: cID,
+            roles: roles
+        })
+            .then(response => response.data)
     }
 }
