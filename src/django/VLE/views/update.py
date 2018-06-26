8b389ede0f4d4c73cd1e5890dea3f85d0b99dc45
Rick <rickw320@hotmail.com>
2018-06-26 13:05:39 +0200

    Responsified the rest of the apis, fixed typos.

--- src/django/VLE/views/update.py
+++ src/django/VLE/views/update.py
@@ -5,8 +5,8 @@ API functions that handle the update requests.
 """
 from rest_framework.decorators import api_view, parser_classes
 from rest_framework.parsers import JSONParser
-from django.http import JsonResponse
 
+import VLE.views.responses as responses
 import VLE.serializers as serialize
 import VLE.utils as utils
 import VLE.factory as factory
@@ -26,18 +26,18 @@ def update_course(request):
         abbr -- abbreviation of the course
         startdate -- date when the course starts
 
-    Returns a json string for if it is succesful or not.
+    Returns a json string for if it is successful or not.
     """
     user = request.user
     if not user.is_authenticated:
-        return JsonResponse({'result': '401 Authentication Error'}, status=401)
+        return responses.unauthorized()
 
     course = Course.objects.get(pk=request.data['cID'])
     course.name = request.data['name']
     course.abbreviation = request.data['abbr']
     course.startdate = request.data['startDate']
     course.save()
-    return JsonResponse({'result': 'success', 'course': serialize.course_to_dict(course)}, status=200)
+    return responses.success(payload={'course': serialize.course_to_dict(course)})
 
 
 @api_view(['POST'])
@@ -49,26 +49,25 @@ def update_course_with_studentID(request):
         uID -- student ID given with the request
         cID -- course ID given with the request
 
-    Returns a json string for if it is succesful or not.
+    Returns a json string for if it is successful or not.
     """
     user = request.user
     if not user.is_authenticated:
-        return JsonResponse({'result': '401 Authentication Error'}, status=401)
+        return responses.unauthorized()
 
     try:
         user = User.objects.get(pk=request.data['uID'])
         course = Course.objects.get(pk=request.data['cID'])
 
     except (User.DoesNotExist, Course.DoesNotExist, Participation.DoesNotExist):
-        return JsonResponse({'result': '404 Not Found',
-                             'description': 'User, Course or Participation does not exist.'}, status=404)
+        return responses.not_found('User, Course or Participation does not exist.')
 
-    #  TODO use roles from course
+    # TODO use roles from course
     role = Role.objects.get(name="Student")
     participation = factory.make_participation(user, course, role)
 
     participation.save()
-    return JsonResponse({'result': 'Succesfully added student to course'}, status=200)
+    return responses.success(message='Succesfully added student to course')
 
 
 @api_view(['POST'])
@@ -81,19 +80,18 @@ def update_assignment(request):
         description -- description of the assignment
         deadline -- deadline of the assignment
 
-    Returns a json string for if it is succesful or not.
+    Returns a json string for if it is successful or not.
     """
     user = request.user
     if not user.is_authenticated:
-        return JsonResponse({'result': '401 Authentication Error'}, status=401)
+        return responses.unauthorized()
 
     assignment = Assignment.objects.get(pk=request.data['aID'])
     assignment.name = request.data['name']
     assignment.description = request.data['description']
     assignment.save()
 
-    return JsonResponse({'result': 'success', 'assignment': serialize.assignment_to_dict(assignment)},
-                        status=200)
+    return responses.success(payload={'assignment': serialize.assignment_to_dict(assignment)})
 
 
 @api_view(['POST'])
@@ -105,32 +103,23 @@ def update_password(request):
         new_password -- new password of the user
         old_password -- current password of the user
 
-    Returns a json string for if it is succesful or not.
+    Returns a json string for if it is successful or not.
     """
     user = request.user
     if not user.is_authenticated or not user.check_password(request.data['old_password']):
-        return JsonResponse({'result': '401 Authentication Error', 'description': 'Wrong password'}, status=401)
+        return responses.unauthorized('Wrong password')
 
     password = request.data['new_password']
     if len(password) < 8:
-        return JsonResponse({
-            'result': '400 Bad request',
-            'description': 'Password needs to contain at least 8 characters'
-        }, status=400)
+        return responses.bad_request('Password needs to contain at least 8 characters')
     if password == password.lower():
-        return JsonResponse({
-            'result': '400 Bad request',
-            'description': 'Password needs to contain at least 1 capital letter'
-        }, status=400)
+        return responses.bad_request('Password needs to contain at least 1 capital letter')
     if re.match(r'^\w+$', password):
-        return JsonResponse({
-            'result': '400 Bad request',
-            'description': 'Password needs to contain a special character'
-        }, status=400)
+        return responses.bad_request('Password needs to contain a special character')
 
     user.set_password(password)
     user.save()
-    return JsonResponse({'result': 'success', 'description': 'Succesfully changed the password'}, status=200)
+    return responses.success(message='Succesfully changed the password')
 
 
 @api_view(['POST'])
@@ -140,19 +129,19 @@ def update_grade_notification(request):
     Arguments:
     request -- the request that was send with
 
-    Returns a json string for if it is succesful or not.
+    Returns a json string for if it is successful or not.
     """
     user = request.user
     if not user.is_authenticated:
-        return JsonResponse({'result': '401 Authentication Error'}, status=401)
+        return responses.unauthorized()
 
     try:
         user.grade_notifications = request.data['new_value']
     except Exception:
-        return JsonResponse({'result': '400 Bad Request'}, status=400)
+        return responses.bad_request()
 
     user.save()
-    return JsonResponse({'result': 'success', 'new_value': user.grade_notifications}, status=200)
+    return responses.bad_request(payload={'new_value': user.grade_notifications})
 
 
 @api_view(['POST'])
@@ -162,19 +151,19 @@ def update_comment_notification(request):
     Arguments:
     request -- the request that was send with
 
-    Returns a json string for if it is succesful or not.
+    Returns a json string for if it is successful or not.
     """
     user = request.user
     if not user.is_authenticated:
-        return JsonResponse({'result': '401 Authentication Error'}, status=401)
+        return responses.unauthorized()
 
     try:
         user.comment_notifications = request.data['new_value']
     except Exception:
-        return JsonResponse({'result': '400 Bad Request'}, status=400)
+        return responses.bad_request()
 
     user.save()
-    return JsonResponse({'result': 'success', 'new_value': user.comment_notifications}, status=200)
+    return responses.success(payload={'new_value': user.comment_notifications})
 
 
 def update_templates(result_list, templates):
@@ -261,9 +250,7 @@ def update_presets(assignment, presets):
             try:
                 preset_node = PresetNode.objects.get(pk=preset['pID'])
             except EntryTemplate.DoesNotExist:
-                return JsonResponse({'result': '404 Not Found',
-                                     'description': 'Preset does not exist.'},
-                                    status=404)
+                return responses.not_found('Preset does not exist.')
         else:
             preset_node = PresetNode(format=format)
 
@@ -299,24 +286,22 @@ def update_format(request):
                             deck, but are not used in presets nor the entry templates.
     """
     if not request.user.is_authenticated:
-        return JsonResponse({'result': '401 Authentication Error'}, status=401)
+        return responses.unauthorized()
 
     try:
-        aID, templates, presets, unused_templates = utils.get_required_post_params(request.data,
-                                                                                   "aID",
-                                                                                   "templates",
-                                                                                   "presets",
-                                                                                   "unused_templates")
+        aID, templates, presets, unused_templates = utils.required_params(request.data,
+                                                                          "aID",
+                                                                          "templates",
+                                                                          "presets",
+                                                                          "unused_templates")
     except KeyError:
-        return utils.keyerror_json("aID", "templates", "presets", "unused_templates")
+        return responses.keyerror("aID", "templates", "presets", "unused_templates")
 
     try:
         assignment = Assignment.objects.get(pk=aID)
         format = assignment.format
     except Assignment.DoesNotExist:
-        return JsonResponse({'result': '404 Not Found',
-                             'description': 'Format does not exist.'},
-                            status=404)
+        return responses.not_found('Format does not exist.')
 
     update_presets(assignment, presets)
     update_templates(format.available_templates, templates)
@@ -327,7 +312,7 @@ def update_format(request):
     swap_templates(format.available_templates, unused_templates, format.unused_templates)
     swap_templates(format.unused_templates, templates, format.available_templates)
 
-    return JsonResponse({'result': 'success', 'format': serialize.format_to_dict(format)}, status=200)
+    return responses.success(payload={'format': serialize.format_to_dict(format)})
 
 
 @api_view(['POST'])
@@ -337,22 +322,21 @@ def update_user_role_course(request):
     Arguments:
     request -- the request that was send with
 
-    Returns a json string for if it is succesful or not.
+    Returns a json string for if it is successful or not.
     """
     try:
-        uID, cID = utils.get_required_post_params(request.data, "uID", "cID")
+        uID, cID = utils.required_params(request.data, "uID", "cID")
     except KeyError:
-        return utils.keyerror_json("uID", "cID")
+        return responses.keyerror("uID", "cID")
 
     try:
         participation = Participation.objects.get(user=request.data['uID'], course=request.data['cID'])
         participation.role = Role.objects.get(name=request.data['role'])
     except (Participation.DoesNotExist, Role.DoesNotExist):
-        return JsonResponse({'result': '404 Not Found',
-                             'description': 'Participation or Role does not exist.'}, status=404)
+        return responses.not_found('Participation or Role does not exist.')
 
     participation.save()
-    return JsonResponse({'result': 'success', 'new_role': participation.role.name}, status=200)
+    return responses.success(payload={'new_role': participation.role.name})
 
 
 @api_view(['POST'])
@@ -365,16 +349,16 @@ def update_grade_entry(request, eID):
     published -- published
     eID -- the entry id
 
-    Returns a json string if it was sucessful or not.
+    Returns a json string if it was successful or not.
     """
     if not request.user.is_authenticated:
-        return JsonResponse({'result': '401 Authentication Error'}, status=401)
+        return responses.unauthorized()
 
     entry = Entry.objects.get(pk=eID)
     entry.grade = request.data['grade']
     entry.published = request.data['published']
     entry.save()
-    return JsonResponse({'result': 'success', 'new_grade': entry.grade, 'new_published': entry.published}, status=200)
+    return responses.success(payload={'new_grade': entry.grade, 'new_published': entry.published})
 
 
 @api_view(['POST'])
@@ -385,16 +369,16 @@ def update_publish_grade_entry(request, eID):
     request -- the request that was send with
     eID -- the entry id
 
-    Returns a json string if it was sucessful or not.
+    Returns a json string if it was successful or not.
     """
     if not request.user.is_authenticated:
-        return JsonResponse({'result': '401 Authentication Error'}, status=401)
+        return responses.unauthorized()
 
     publish = request.data['published']
     entry = Entry.objects.get(pk=eID)
     entry.published = publish
     entry.save()
-    return JsonResponse({'result': 'success', 'new_published': entry.published}, status=200)
+    return responses.success(payload={'new_published': entry.published})
 
 
 @api_view(['POST'])
@@ -405,14 +389,14 @@ def update_publish_grades_assignment(request, aID):
     request -- the request that was send with
     aID -- assignment ID
 
-    Returns a json string if it was sucessful or not.
+    Returns a json string if it was successful or not.
     """
     if not request.user.is_authenticated:
-        return JsonResponse({'result': '401 Authentication Error'}, status=401)
+        return responses.unauthorized()
 
     assign = Assignment.objects.get(pk=aID)
     utils.publish_all_assignment_grades(assign, request.data['published'])
-    return JsonResponse({'result': 'success', 'new_published': request.data['published']}, status=200)
+    return responses.success(payload={'new_published': request.data['published']})
 
 
 @api_view(['POST'])
@@ -424,14 +408,14 @@ def update_publish_grades_journal(request, jID):
         published -- publish state of grade
     jID -- journal ID
 
-    Returns a json string if it was sucessful or not.
+    Returns a json string if it was successful or not.
     """
     if not request.user.is_authenticated:
-        return JsonResponse({'result': '401 Authentication Error'}, status=401)
+        return responses.unauthorized()
 
     journ = Journal.objects.get(pk=jID)
     utils.publish_all_journal_grades(journ, request.data['published'])
-    return JsonResponse({'result': 'success', 'new_published': request.data['published']}, status=200)
+    return responses.success(payload={'new_published': request.data['published']})
 
 
 @api_view(['POST'])
@@ -443,25 +427,23 @@ def update_entrycomment(request):
     request -- the request that was send with
         entrycommentID -- The ID of the entrycomment.
         text -- The updated text.
-    Returns a json string for if it is succesful or not.
+    Returns a json string for if it is successful or not.
     """
     if not request.user.is_authenticated:
-        return JsonResponse({'result': '401 Authentication Error'}, status=401)
+        return responses.unauthorized()
 
     try:
-        entrycommentID, text = utils.get_required_post_params(request.data, "entrycommentID", "text")
+        entrycommentID, text = utils.required_params(request.data, "entrycommentID", "text")
     except KeyError:
-        return utils.keyerror_json("entrycommentID")
+        return responses.keyerror("entrycommentID")
 
     try:
         comment = EntryComment.objects.get(pk=entrycommentID)
     except EntryComment.DoesNotExist:
-        return JsonResponse({'result': '404 Not Found',
-                             'description': 'Entrycomment does not exist.'},
-                            status=404)
+        return responses.not_found('Entrycomment does not exist.')
     comment.text = text
     comment.save()
-    return JsonResponse({'result': 'success'}, status=200)
+    return responses.success()
 
 
 @api_view(['POST'])
@@ -473,11 +455,11 @@ def update_user_data(request):
         username -- new password of the user
         picture -- current password of the user
 
-    Returns a json string for if it is succesful or not.
+    Returns a json string for if it is successful or not.
     """
     user = request.user
     if not user.is_authenticated:
-        return JsonResponse({'result': '401 Authentication Error'}, status=401)
+        return responses.unauthorized()
 
     if 'username' in request.data:
         user.username = request.data['username']
@@ -485,4 +467,4 @@ def update_user_data(request):
         user.profile_picture = request.data['picture']
 
     user.save()
-    return JsonResponse({'result': 'success', 'user': serialize.user_to_dict(user)}, status=200)
+    return responses.success(payload={'user': serialize.user_to_dict(user)})
