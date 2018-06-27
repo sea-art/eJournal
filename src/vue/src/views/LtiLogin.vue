bfecd18aeafb0487c98b761679bb78cb2ded4dab
Okke van Eck <okke.van.eck@gmail.com>
2018-06-27 15:10:49 +0200

    Changed typo

--- src/vue/src/views/LtiLogin.vue
+++ src/vue/src/views/LtiLogin.vue
@@ -34,7 +34,7 @@ export default {
                 firstname: '',
                 lastname: '',
                 username: '',
-                emaik: ''
+                email: ''
             }
         }
     },
