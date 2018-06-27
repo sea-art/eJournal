7d771eeefc708da63a05d93cb71da18a77c5f889
DennisWind <dennisgw@live.nl>
2018-06-27 16:54:58 +0200

    Maarten requested change

--- src/django/VLE/factory.py
+++ src/django/VLE/factory.py
@@ -58,7 +58,7 @@ def make_course(name, abbrev, startdate=None, author=None, lti_id=None):
     course = Course(name=name, abbreviation=abbrev, startdate=startdate, author=author, lti_id=lti_id)
     course.save()
 
-    # Het scheelt werk voor lti als de Student en TA van een vak altijd bestaan.
+    # Student, TA and Teacher role are created on course creation as is saves check for lti.
     make_role_student("Student", course)
     make_role_ta("TA", course)
     role = make_role_teacher("Teacher", course)
