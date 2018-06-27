340ec4104b71fbee44731417193c59649190de73
Okke van Eck <okke.van.eck@gmail.com>
2018-06-27 12:31:26 +0200

    Make setup brok?

--- src/vue/src/components/CreateUser.vue
+++ src/vue/src/components/CreateUser.vue
@@ -1,13 +1,6 @@
 <template>
     <div>
-        <register-user/>
-
-        <!-- <b-form @submit="onSubmit" @reset="onReset">
-            <b-input class="mb-2 mr-sm-2 mb-sm-0 multi-form" v-model="form.username" placeholder="Username" required/>
-            <b-input class="mb-2 mr-sm-2 mb-sm-0 multi-form" v-model="form.password" placeholder="Password" required/>
-            <b-button class="float-right" type="reset">Reset</b-button>
-            <b-button class="float-right" type="submit">Create</b-button>
-        </b-form> -->
+        <register-user :lti="lti"/>
     </div>
 </template>
 
