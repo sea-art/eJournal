5e7ba10884b0db3919aa7e814be7df7236fd05ed
Maarten van Keulen <mjvkeulen1@gmail.com>
2018-06-29 09:48:44 +0200

    Guest page

--- src/vue/src/views/Guest.vue
+++ src/vue/src/views/Guest.vue
@@ -1,6 +1,13 @@
 <template>
     <content-single-columns>
-        <h1>Placeholder for the Guest page (Landing page)</h1>
+        <h1>Welcome to eJournal!</h1>
+        <h5>Please enjoy a brief tour of our website:</h5><br/>
+        <img src="../../static/landing_page/course_overview.png">
+        <img src="../../static/landing_page/journal.png">
+        <img src="../../static/landing_page/course_management.png">
+        <img src="../../static/landing_page/template_editor.png">
+        <img src="../../static/landing_page/format_editor.png">
+        <img src="../../static/landing_page/role_configuration.png">
     </content-single-columns>
 </template>
 
