9ac0b5466f179c71b1ca70a0a30a212b7b04340a
Okke van Eck <okke.van.eck@gmail.com>
2018-07-01 22:43:38 +0200

    Added front-end checks and changed suggested changes for the pull request

--- src/vue/src/components/RegisterUser.vue
+++ src/vue/src/components/RegisterUser.vue
@@ -6,9 +6,10 @@
                 <b-input class="mb-2 mr-sm-2 mb-sm-0 multi-form"  v-model="form.firstname" placeholder="Firstname" required/>
                 <b-input class="mb-2 mr-sm-2 mb-sm-0 multi-form"  v-model="form.lastname" placeholder="Lastname" required/>
                 <b-input class="mb-2 mr-sm-2 mb-sm-0 multi-form" v-model="form.password" type="password" placeholder="Password" required/>
+                <b-input class="mb-2 mr-sm-2 mb-sm-0 multi-form" v-model="form.password2" type="password" placeholder="Password (again)" required/>
                 <b-input class="mb-2 mr-sm-2 mb-sm-0 multi-form"  v-model="form.email" placeholder="Email" required/>
                 <b-button class="float-right" type="reset">Reset</b-button>
-                <b-button class="float-right" type="submit">Create</b-button>
+                <b-button class="float-right" type="submit">Register</b-button>
             </b-form>
         </b-card>
     </div>
@@ -26,6 +27,7 @@ export default {
             form: {
                 username: '',
                 password: '',
+                password2: '',
                 firstname: '',
                 lastname: '',
                 email: '',
@@ -34,38 +36,65 @@ export default {
         }
     },
     methods: {
+        checkInput () {
+            var correctInput = true
+
+            /* Validate the password. */
+            var re = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/
+            if (!re.test(String(this.form.password))) {
+                this.$toasted.error('The given password does not match the criteria!')
+                correctInput = false
+            }
+            if (this.form.password !== this.form.password2) {
+                this.$toasted.error('The given passwords do not match!')
+                correctInput = false
+            }
+
+            /* Validate the email. */
+            re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+            if (!re.test(String(this.form.email).toLowerCase())) {
+                this.$toasted.error('The given email is incorrect!')
+                correctInput = false
+            }
+
+            return correctInput
+        },
         onSubmit () {
-            userApi.createUser(this.form.username, this.form.password,
-                this.form.firstname, this.form.lastname,
-                this.form.email, this.form.ltiJWT)
-                .then(_ => {
-                    auth.login(this.form.username, this.form.password)
-                        .then(_ => {
-                            this.$emit('handleAction')
-                        })
-                        .catch(_ => {
-                            this.$router.push({
-                                name: 'ErrorPage',
-                                params: {
-                                    code: '511',
-                                    message: 'Network authorization required',
-                                    description: `Invalid credentials for logging in.
-                                                  Please contact your system administrator.`
-                                }
+            var correctInput = this.checkInput()
+
+            if (correctInput) {
+                userApi.createUser(this.form.username, this.form.password,
+                    this.form.firstname, this.form.lastname,
+                    this.form.email, this.form.ltiJWT)
+                    .then(_ => {
+                        auth.login(this.form.username, this.form.password)
+                            .then(_ => {
+                                this.$emit('handleAction')
                             })
-                        })
-                })
-                .catch(_ => {
-                    this.$router.push({
-                        name: 'ErrorPage',
-                        params: {
-                            code: '500',
-                            message: 'Internal Server Error',
-                            description: `Could not create user. Please contact
-                                          your system administrator.`
-                        }
+                            .catch(_ => {
+                                this.$router.push({
+                                    name: 'ErrorPage',
+                                    params: {
+                                        code: '511',
+                                        message: 'Network authorization required',
+                                        description: `Invalid credentials for logging in.
+                                                      Please contact your system administrator.`
+                                    }
+                                })
+                            })
                     })
-                })
+                    .catch(_ => {
+                        this.$router.push({
+                            name: 'ErrorPage',
+                            params: {
+                                code: '500',
+                                message: 'Internal Server Error',
+                                description: `Could not create user. Please contact
+                                              your system administrator.`
+                            }
+                        })
+                    })
+            }
         },
         onReset (evt) {
             if (evt !== undefined) {
