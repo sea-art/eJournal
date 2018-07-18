43c5a6de3e208a32b86b68ee8bcadeea68336bb5
Lars van Hijfte <larsvanhijfte@hotmail.com>
2018-07-18 14:56:10 +0200

    Improved tests and added roles create & update

--- src/django/VLE/views/roles.py
+++ src/django/VLE/views/roles.py
@@ -3,10 +3,11 @@ import VLE.permissions as permissions
 import VLE.views.responses as response
 from VLE.serializers import RoleSerializer
 from VLE.models import Course, Role
+import VLE.factory as factory
 
 
 class RoleView(object):
-    def list(self, request, pk):
+    def list(request, pk):
         """Get course roles.
 
         Arguments:
@@ -20,7 +21,7 @@ class RoleView(object):
             forbidden -- when the user is not in the course
             forbidden -- when the user is unauthorized to edit its roles
         On success:
-            success -- list of all the roles in a course
+            success -- list of all the roles in the course
         """
         if not request.user.is_authenticated:
             return response.unauthorized()
@@ -39,8 +40,53 @@ class RoleView(object):
         serializer = RoleSerializer(roles, many=True)
         return response.success(serializer.data)
 
-    def partial_update(self, request, pk):
+    def partial_update(request, pk):
         """Updates course roles.
+
+        Arguments:
+        request -- request data
+            roles -- each role of the course that needs to be updated
+        pk -- course ID
+
+        Returns:
+        On failure:
+            unauthorized -- when the user is not logged in
+            not found -- when the course does not exists
+            forbidden -- when the user is unauthorized to edit its roles
+        On success:
+            success -- list of all the roles in the course
+        """
+        if not request.user.is_authenticated:
+            return response.unauthorized()
+
+        try:
+            course = Course.objects.get(pk=pk)
+        except Course.DoesNotExist:
+            return response.not_found('course')
+
+        own_role = permissions.get_role(request.user, course)
+        if own_role is None:
+            return response.forbidden('You are not in this course.')
+        elif not own_role.can_edit_course_roles:
+            return response.forbidden('You cannot edit roles of this course.')
+
+        if 'roles' not in request.data:
+            return response.keyerror('roles')
+
+        resp = []
+        for new_role in request.data['roles']:
+            if 'name' not in new_role:
+                return response.keyerror('roles.name')
+            role = Role.objects.get(name=new_role['name'], course=course)
+            serializer = RoleSerializer(role, data=new_role, partial=True)
+            if not serializer.is_valid():
+                response.bad_request()
+            serializer.save()
+            resp.append(serializer.data)
+        return response.success(resp)
+
+    def create(request, pk):
+        """Create course role.
 
         Arguments:
         request -- request data
@@ -52,7 +98,7 @@ class RoleView(object):
             not found -- when the course does not exists
             forbidden -- when the user is unauthorized to edit its roles
         On success:
-            success -- list of all the roles in a course
+            success -- newly created course
         """
         user = request.user
         if not user.is_authenticated:
@@ -63,21 +109,15 @@ class RoleView(object):
         except Course.DoesNotExist:
             return response.not_found('course')
 
-        role = permissions.get_role(user, course)
-        if role is None:
+        own_role = permissions.get_role(user, course)
+        if own_role is None:
             return response.forbidden('You are not in this course.')
-        elif not role.can_edit_course_roles:
-            return response.forbidden('You cannot edit roles of this course.')
+        elif not own_role.can_edit_course_roles:
+            return response.forbidden('You cannot create roles of this course.')
 
-        serializer = self.serializer_class(course, data=request.data, partial=True)
-        if not serializer.is_valid():
-            response.bad_request()
-        serializer.save()
-        return response.success(serializer.data)
-        # for role in request.data['roles']:
-        #     db_role = Role.objects.filter(name=role['name'])
-        #     if not db_role:
-        #         factory.make_role_default_no_perms(role['name'], Course.objects.get(pk=cID), **role['permissions'])
-        #     else:
-        #         permissions.edit_permissions(db_role[0], **role['permissions'])
-        # return responses.success()
+        try:
+            role = factory.make_role_default_no_perms(request.data['name'], course, **request.data['permissions'])
+        except Exception:
+            return response.bad_request()
+        serializer = RoleSerializer(role, many=False)
+        return response.created(serializer.data, obj='role')
