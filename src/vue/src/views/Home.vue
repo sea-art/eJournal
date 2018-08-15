894d9fd54277878f69e696ec6104e18dd077cf91
Maarten van Keulen <mjvkeulen1@gmail.com>
2018-08-15 14:19:54 +0200

    Removal of test component

--- src/vue/src/views/Home.vue
+++ src/vue/src/views/Home.vue
@@ -7,8 +7,6 @@
             :currentPage="'Courses'">
         </bread-crumb>
 
-        <pdf-display slot="main-content-column" :fileName="'Eerste_response_van_wijk.pdf'"/>
-
         <div v-for="c in courses" :key="c.cID" slot="main-content-column">
             <b-link :to="{name: 'Course', params: {cID: c.cID, courseName: c.name}}">
                 <main-card
@@ -82,8 +80,6 @@ import icon from 'vue-awesome/components/Icon'
 import course from '@/api/course'
 import assignmentApi from '@/api/assignment.js'
 
-import pdfDisplay from '@/components/assets/PdfDisplay.vue'
-
 export default {
     name: 'Home',
     data () {
@@ -101,8 +97,7 @@ export default {
         'todo-card': todoCard,
         'create-course': createCourse,
         'edit-home': editHome,
-        'icon': icon,
-        'pdf-display': pdfDisplay
+        'icon': icon
     },
     created () {
         this.loadCourses()
