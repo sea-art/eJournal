4c723df64746aa5550dc4224f2e6e220e4c4ed04
Jeroen van Bennekum <jeroen_-@live.nl>
2018-08-25 22:27:37 +0200

    Fixed some code variable names.

--- src/django/VLE/views/node.py
+++ src/django/VLE/views/node.py
@@ -35,7 +35,7 @@ class NodeView(viewsets.ModelViewSet):
 
         Arguments:
         request -- request data
-            jID -- journal ID
+            journal_id -- journal ID
 
         Returns:
         On failure:
@@ -50,9 +50,9 @@ class NodeView(viewsets.ModelViewSet):
             return response.unauthorized()
 
         try:
-            journal_id = request.query_params['jID']
+            journal_id = request.query_params['journal_id']
         except KeyError:
-            return response.keyerror('jID')
+            return response.keyerror('journal_id')
 
         try:
             journal = Journal.objects.get(pk=journal_id)
@@ -67,62 +67,5 @@ class NodeView(viewsets.ModelViewSet):
         can_add = journal.user == request.user
         can_add = can_add and \
             permissions.has_assignment_permission(request.user, journal.assignment, 'can_edit_journal')
-
-        nodes = journal.node_set.annotate(
-            sort_deadline=Case(
-                When(type=Node.ENTRY, then='entry__createdate'),
-                default='preset__deadline')
-        ).order_by('sort_deadline')
-        node_dict = []
-        added_add_node = False
-        for node in nodes:
-            if node.type == Node.PROGRESS:
-                is_future = (node.preset.deadline - datetime.now()).total_seconds() > 0
-                if can_add and not added_add_node and is_future:
-                    add_node = JournalSerializer(journal).data
-                    if add_node is not None:
-                        node_dict.append(add_node)
-                    added_add_node = True
-            node_dict.append(self.serializer_class(node).data)
-        return response.success(node_dict)
-
-    def retrieve(request, jID):
-        """Retrieve an assignment.
-
-        Arguments:
-        request -- request data
-            lti -- if this is set, the pk is an lti_id, not a 'normal' id
-        pk -- assignment ID
-
-        Returns:
-        On failure:
-            unauthorized -- when the user is not logged in
-            not_found -- could not find the course with the given id
-            forbidden -- not allowed to retrieve assignments in this course
-
-        On success:
-            succes -- with the assignment data
-
-        """
-        """Get all nodes contained within a journal.
-
-        Arguments:
-        request -- the request that was sent
-        jID     -- the journal id
-
-        Returns a json string containing all entry and deadline nodes.
-        """
-        user = request.user
-        if not user.is_authenticated:
-            return response.unauthorized()
-
-        try:
-            journal = Journal.objects.get(pk=jID)
-        except Journal.DoesNotExist:
-            return response.not_found('Journal')
-
-        if not (journal.user is user or permissions.has_assignment_permission(user,
-                journal.assignment, 'can_view_assignment_participants')):
-            return response.forbidden('You are not allowed to view journals of other participants.')
 
         return response.success(payload={'nodes': edag.get_nodes_dict(journal, request.user)})
