0c90a274e4a740bd4a291cb254b156e8ae81a219
Joey Lai <joeylai96@hotmail.com>
2018-09-05 21:24:30 +0200

    Added url for group apis

--- src/django/VLE/views/urls.py
+++ src/django/VLE/views/urls.py
@@ -1,5 +1,6 @@
 from rest_framework import routers
 from VLE.views.course import CourseView
+from VLE.views.group import GroupView
 from VLE.views.role import RoleView
 from VLE.views.user import UserView
 from VLE.views.assignment import AssignmentView
@@ -12,6 +13,7 @@ from VLE.views.entry import EntryView
 
 router = routers.DefaultRouter()
 router.register(r'courses', CourseView, base_name='course')
+router.register(r'groups', GroupView, base_name='group')
 router.register(r'roles', RoleView, base_name='role')
 router.register(r'users', UserView, base_name='user')
 router.register(r'assignments', AssignmentView, base_name='assignment')
