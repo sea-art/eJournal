15a0cb8397ec09f8d8778a79cfb66e71308aa71b
Lars van Hijfte <larsvanhijfte@hotmail.com>
2018-09-05 22:40:42 +0200

    Removed old code

--- src/vue/src/api/format.js
+++ src/vue/src/api/format.js
@@ -15,15 +15,5 @@ export default {
     // create_template (name, fields) {
     //     return auth.authenticatedPost('/create_template/', {name: name, fields: fields})
     //         .then(response => response.data)
-    // },
-    //
-    // get_format (aID) {
-    //     return auth.authenticatedGet('/get_format/' + aID + '/')
-    //         .then(response => response.data)
-    // },
-    //
-    // update_format (aID, templates, maxPoints, presets, unusedTemplates, removedTemplates, removedPresets) {
-    //     return auth.authenticatedPost('/update_format/', {aID: aID, templates: templates, max_points: maxPoints, presets: presets, unused_templates: unusedTemplates, removed_templates: removedTemplates, removed_presets: removedPresets})
-    //         .then(response => response.data)
-    // },
+    // }
 }
