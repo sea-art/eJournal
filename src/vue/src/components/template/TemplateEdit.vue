c90eb49bfc03cc2f3eed40777116458e6b282be9
Engel Hamer <engelhamer@gmail.com>
2018-09-07 13:45:51 +0200

    Removed unnecessary wrappers

--- src/vue/src/components/template/TemplateEdit.vue
+++ src/vue/src/components/template/TemplateEdit.vue
@@ -1,40 +1,36 @@
 <template>
-    <div>
-        <b-card class="no-hover">
-            <b-input class="mb-2 mr-sm-2 mb-sm-0 multi-form theme-input" id="templateName" v-model="template.name" placeholder="Template name" required/>
-            <draggable v-model="template.field_set" @start="drag=true" @end="drag=false" @update="onUpdate" :options="{ handle:'.handle' }">
-                <div v-for="field in template.field_set" :key="field.location">
-                    <b-card class="field-card">
-                        <b-row align-h="between" no-gutters>
-                            <b-col cols="12" sm="10" lg="11">
-                                <b-input class="mb-2 mr-sm-2 mb-sm-0 multi-form theme-input" v-model="field.title" placeholder="Field title" required/>
-                                <b-select class="multi-form" :options="fieldTypes" v-model="field.type"></b-select>
-                                <b-button v-on:click.stop v-if="!field.required" @click="field.required = !field.required" class="optional">
-                                    <icon name="times"/>
-                                    Optional
-                                </b-button>
-                                <b-button v-on:click.stop v-if="field.required" @click="field.required = !field.required" class="required">
-                                    <icon name="check"/>
-                                    Required
-                                </b-button><br>
-                            </b-col>
-                            <b-col cols="12" sm="2" lg="1" class="icon-box">
-                                <div class="handle d-inline d-sm-block">
-                                    <icon class="move-icon" name="arrows" scale="1.75"/>
-                                </div>
-                                <icon class="trash-icon" @click.native="removeField(field.location)" name="trash" scale="1.75"/>
-                            </b-col>
-                        </b-row>
-                    </b-card>
-                </div>
-                <div class="invisible"></div>
-            </draggable>
-            <b-button class="add-button full-width" @click="addField">
-                <icon name="plus"/>
-                Add field
-            </b-button>
-        </b-card>
-    </div>
+    <b-card class="no-hover">
+        <b-input class="mb-2 mr-sm-2 mb-sm-0 multi-form theme-input" id="templateName" v-model="template.name" placeholder="Template name" required/>
+        <draggable v-model="template.field_set" @start="drag=true" @end="drag=false" @update="onUpdate" :options="{ handle:'.handle' }">
+            <b-card v-for="field in template.field_set" :key="field.location" class="field-card">
+                <b-row align-h="between" no-gutters>
+                    <b-col cols="12" sm="10" lg="11">
+                        <b-input class="mb-2 mr-sm-2 mb-sm-0 multi-form theme-input" v-model="field.title" placeholder="Field title" required/>
+                        <b-select class="multi-form" :options="fieldTypes" v-model="field.type"></b-select>
+                        <b-button v-on:click.stop v-if="!field.required" @click="field.required = !field.required" class="optional">
+                            <icon name="times"/>
+                            Optional
+                        </b-button>
+                        <b-button v-on:click.stop v-if="field.required" @click="field.required = !field.required" class="required">
+                            <icon name="check"/>
+                            Required
+                        </b-button><br>
+                    </b-col>
+                    <b-col cols="12" sm="2" lg="1" class="icon-box">
+                        <div class="handle d-inline d-sm-block">
+                            <icon class="move-icon" name="arrows" scale="1.75"/>
+                        </div>
+                        <icon class="trash-icon" @click.native="removeField(field.location)" name="trash" scale="1.75"/>
+                    </b-col>
+                </b-row>
+            </b-card>
+            <div class="invisible"></div>
+        </draggable>
+        <b-button class="add-button full-width" @click="addField">
+            <icon name="plus"/>
+            Add field
+        </b-button>
+    </b-card>
 </template>
 
 <script>
