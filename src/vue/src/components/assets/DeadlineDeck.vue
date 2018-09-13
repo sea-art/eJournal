4601b45de2c51b9c5b28e4ac158062a6b7efbaab
Joey Lai <joeylai96@hotmail.com>
2018-09-13 14:08:26 +0200

    Created deadline deck and added time left functionality for deadlines

---
+++ src/vue/src/components/assets/DeadlineDeck.vue
@@ -0,0 +1,100 @@
+<template>
+    <div>
+        <!-- TODO: This seems like an inappropriate permission check. Will have to be reconsidered in the rework. -->
+        <b-card v-if="$hasPermission('can_add_course')"
+                class="no-hover">
+            <b-form-select v-model="selectedSortOption" :select-size="1">
+                <option value="sortDate">Sort by date</option>
+                <option value="sortNeedsMarking">Sort by marking needed</option>
+            </b-form-select>
+        </b-card>
+
+        <div v-if="$route.path == '/Home'" v-for="(d, i) in computedDeadlines" :key="i">
+                <b-link tag="b-button" :to="assignmentRoute(d.course.id, d.id, d.journal)">
+                <todo-card :deadline="d"/>
+            </b-link>
+        </div>
+
+    </div>
+</template>
+
+<script>
+import todoCard from '@/components/assets/TodoCard.vue'
+import todoSquare from '@/components/assets/TodoSquare.vue'
+export default {
+    props: ['deadlines'],
+    data () {
+        return {
+            selectedSortOption: 'sortDate'
+        }
+    },
+    components: {
+        'todo-card': todoCard,
+        'todo-square': todoSquare
+    },
+    methods: {
+        assignmentRoute (cID, aID, jID) {
+            var route = {
+                name: 'Assignment',
+                params: {
+                    cID: cID,
+                    aID: aID
+                }
+            }
+            if (jID) {
+                route.params.jID = jID
+                route.name = 'Journal'
+            }
+
+            return route
+        },
+        assignmentRouteCourse (cID, aID, jID) {
+            var route = {
+                params: {
+                    cID: cID,
+                    aID: aID
+                }
+            }
+            // TODO Permission revision can_grade
+            if (this.$hasPermission('can_view_assignment_participants', 'assignment', String(aID))) {
+                route.name = 'Assignment'
+            } else {
+                route.name = 'Journal'
+                route.params.jID = jID
+            }
+            return route
+        }
+    },
+    computed: {
+        computedDeadlines: function () {
+            var counter = 0
+
+            function compareDate (a, b) {
+                return new Date(a.deadline) - new Date(b.deadline)
+            }
+
+            function compareMarkingNeeded (a, b) {
+                if (a.stats.needs_marking + a.stats.unpublished > b.stats.needs_marking + b.stats.unpublished) { return -1 }
+                if (a.stats.needs_marking + a.stats.unpublished < b.stats.needs_marking + b.stats.unpublished) { return 1 }
+                return 0
+            }
+
+            function filterTop () {
+                return (++counter <= 5)
+            }
+
+            function filterNoEntries (deadline) {
+                return deadline.totalNeedsMarking !== 0
+            }
+
+            if (this.selectedSortOption === 'sortDate') {
+                return this.deadlines.slice().sort(compareDate).filter(filterTop)
+            } else if (this.selectedSortOption === 'sortNeedsMarking') {
+                return this.deadlines.slice().sort(compareMarkingNeeded).filter(filterTop).filter(filterNoEntries)
+            } else {
+                return this.deadlines.slice().sort(compareDate).filter(filterTop)
+            }
+        }
+    }
+}
+</script>
