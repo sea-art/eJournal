1a5ad48b524e883dc932ac6c11f2562b46d90f5e
Zi Long Zhu <zilong.zhu31@gmail.com>
2018-09-13 16:18:53 +0200

    added rich text editor, but cannot get the data out.

--- src/vue/src/views/Feedback.vue
+++ src/vue/src/views/Feedback.vue
@@ -2,6 +2,10 @@
     <content-single-column>
         <bread-crumb>&nbsp;</bread-crumb>
         <b-card class="no-hover blue-border">
+            Hi! If you have any suggestions for improvements or found
+            any issues/bugs please inform us and fill in the form below. Thank you!
+        </b-card>
+        <b-card class="no-hover blue-border">
             <b-form-group label="Topic:">
                 <b-input
                     class="theme-input multi-form"
@@ -19,13 +23,13 @@
             </b-form-group>
 
             <b-form-group label="Feedback:">
-                <b-form-textarea
-                    class="theme-input multi-form"
-                    v-model="feedback"
-                    :rows="4"
-                    :max-rows="10"
-                    required
-                />
+                <div class="multi-form">
+                    <text-editor
+                        :id="'rich-text-editor'"
+                        :givenContent="feedback.data"
+                        @content-update="feedback.data = $event"
+                    />
+                </div>
             </b-form-group>
             <b-button @click="sendFeedback" class="add-button float-right">
                 <icon name="save"/>
@@ -38,25 +42,28 @@
 <script>
 import breadCrumb from '@/components/assets/BreadCrumb.vue'
 import contentSingleColumn from '@/components/columns/ContentSingleColumn.vue'
-
+import textEditor from '@/components/assets/TextEditor.vue'
+import icon from 'vue-awesome/components/Icon'
 import feedback from '@/api/feedback'
 
 export default {
     data () {
         return {
             topic: '',
-            feedback: '',
+            feedback: [],
             type: null,
             types: [
                 {text: 'Select one', value: null},
                 'Issue/Bug', 'Suggestion', 'Other'
-            ]
+            ],
+            icon
         }
     },
     name: 'Feedback',
     components: {
         'content-single-column': contentSingleColumn,
-        'bread-crumb': breadCrumb
+        'bread-crumb': breadCrumb,
+        'text-editor': textEditor
     },
     methods: {
         sendFeedback () {
@@ -67,6 +74,9 @@ export default {
             } else if (this.body === '') {
                 this.$toasted.error('Feedback required')
             } else {
+                this.$toasted.error('yo')
+                console.log('feedback: ' + this.feedback)
+                console.log('event:' + this.$event)
                 feedback.sendFeedback(this.topic, this.type, this.feedback, navigator.userAgent)
                     .then(response => { this.$toasted.success(response.data.description) })
                     .catch(error => { this.$toasted.error(error.response.data.description) })
