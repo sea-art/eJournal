25c86ea24be4521438d1c5558e80070a2f2b40c1
Zi Long Zhu <zilong.zhu31@gmail.com>
2018-09-15 20:18:40 +0200

    reply email text changed

--- src/django/VLE/utils/email_handling.py
+++ src/django/VLE/utils/email_handling.py
@@ -55,8 +55,8 @@ def send_email_feedback(user, files, topic, ftype, feedback, user_agent, url):
     r_subject = "[eJournal] Submitted feedback"
     r_body = "Hi {},\n\n".format(user.first_name)
     r_body += "Thank you for your feedback! Below you will find a copy of your given feedback. "
-    r_body += "If you supplied attachments, then they are added to this e-mail aswell.\n\n"
-    r_body += "THE FEEDBACK:\n{}\n\n".format(feedback[0])
+    r_body += "If you supplied attachments, then they are added to this e-mail as well.\n\n"
+    r_body += "The feedback:\n{}\n\n".format(feedback[0])
     r_body += "We might reply to your feedback to ask some questions or just to say thanks!\n\n"
     r_body += "Kind Regards,\n\nThe eJournal Team"
 
