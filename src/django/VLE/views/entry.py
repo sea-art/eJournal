22206a8c28646bcaa1f0e11b09f318e087889792
Lars van Hijfte <larsvanhijfte@hotmail.com>
2018-09-16 21:27:58 +0200

    Flake fixes

--- src/django/VLE/views/entry.py
+++ src/django/VLE/views/entry.py
@@ -3,7 +3,6 @@ entry.py.
 
 In this file are all the entry api requests.
 """
-from datetime import datetime
 from rest_framework import viewsets
 from django.core.exceptions import ValidationError
 from django.utils.timezone import now
