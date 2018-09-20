90f17b430c6b3999b7ed3f96a6c4384ca1d73d17
Joey Lai <joeylai96@hotmail.com>
2018-09-20 11:48:44 +0200

    Added group name to stduent card

--- src/vue/src/components/assignment/StudentCard.vue
+++ src/vue/src/components/assignment/StudentCard.vue
@@ -6,7 +6,8 @@
             </b-col>
             <b-col order="2" cols="9" class="pl-3">
                 <todo-square v-if="numMarkingNeeded > 0 && !hideTodo" class="float-right" :num="numMarkingNeeded"/>
-                <b>{{ student.first_name + ' ' + student.last_name }}</b><br/>
+                <template v-if="student.group"><b>{{ student.first_name + ' ' + student.last_name }}</b> ({{ student.group }})<br/></template>
+                <template v-else><b>{{ student.first_name + ' ' + student.last_name }}</b><br/></template>
                 {{ student.username }}
                 <progress-bar v-if="!fullWidthProgress || $root.mdMax()" :currentPoints="this.stats.acquired_points" :totalPoints="this.stats.total_points"/>
             </b-col>
