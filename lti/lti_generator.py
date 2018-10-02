c43637867268ef67731e2f74694cf90632dfee1b
DennisWind <dennisgw@live.nl>
2018-10-02 14:40:56 +0200

    module secrets used

--- lti/lti_generator.py
+++ lti/lti_generator.py
@@ -1,11 +1,11 @@
-import os
+import secrets
 
 # Generates a new key and secret couple for the lti Oauth communication te key
 # and secrect are generated according to the guidelines given in
 # https://www.oauth.com/oauth2-servers/client-registration/client-id-secret/
 
 print('LTI_KEY:')
-print(os.urandom(16).hex())
+print(secrets.token_hex(16))
 print()
 print('LTI_SECRET:')
-print(os.urandom(32).hex())
+print(secrets.token_hex(32))
