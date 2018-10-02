900a875a018c01e758d18a104dabd72edecd5f4d
DennisWind <dennisgw@live.nl>
2018-10-02 14:37:53 +0200

    comments to generator

--- lti/lti_generator.py
+++ lti/lti_generator.py
@@ -1,6 +1,11 @@
 import os
-print('key')
+
+# Generates a new key and secret couple for the lti Oauth communication te key
+# and secrect are generated according to the guidelines given in
+# https://www.oauth.com/oauth2-servers/client-registration/client-id-secret/
+
+print('LTI_KEY:')
 print(os.urandom(16).hex())
 print()
-print('secret')
+print('LTI_SECRET:')
 print(os.urandom(32).hex())
