0becaffa2fcdb78d5be7502c78685445ecebda2e
Lars van Hijfte <larsvanhijfte@hotmail.com>
2018-10-10 20:52:20 +0200

    Mixed migration

--- src/django/VLE/migrations/0005_presetnode_description.py
+++ src/django/VLE/migrations/0005_presetnode_description.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('VLE', '0004_participation_role_required'),
+        ('VLE', '0004_userfile_more_detailed_paths'),
     ]
 
     operations = [
