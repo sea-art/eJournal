cf1cb88c044536e3aeb573c751f3b3da758b6c9b
Lars van Hijfte <larsvanhijfte@hotmail.com>
2018-10-19 21:07:38 +0200

    successfully

--- src/vue/src/store/modules/user.js
+++ src/vue/src/store/modules/user.js
@@ -137,7 +137,7 @@ const actions = {
         return new Promise((resolve, reject) => {
             connection.conn.get('/users/0/').then(response => {
                 commit(types.HYDRATE_USER, response.data)
-                resolve('Store is populated Successfully')
+                resolve('Store is populated successfully')
             }, error => {
                 Vue.toasted.error(error.response.data.description)
                 reject(error)
