2aa9ded77e42b6a40a920e5ba34557f7cbbdea72
Lars van Hijfte <larsvanhijfte@hotmail.com>
2018-10-22 10:58:07 +0200

    Fixed some LTI issues

--- src/django/VLE/lti_launch.py
+++ src/django/VLE/lti_launch.py
@@ -121,7 +121,8 @@ def check_assignment_lti(request):
     lti_couples = Lti_ids.objects.filter(lti_id=assign_id, for_model=Lti_ids.ASSIGNMENT)
     if lti_couples.count() > 0:
         assignment = lti_couples[0].assignment
-        assignment.is_published = request['custom_assignment_publish']
+        if 'custom_assignment_publish' in request:
+            assignment.is_published = request['custom_assignment_publish'] == 'true'
         assignment.save()
         return assignment
     return None
