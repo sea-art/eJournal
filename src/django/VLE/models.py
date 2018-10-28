9bf3e4ac89a11858e797dcd363d841a556dbeb65
Lars van Hijfte <larsvanhijfte@hotmail.com>
2018-10-28 22:38:00 +0100

    isort

--- src/django/VLE/models.py
+++ src/django/VLE/models.py
@@ -13,11 +13,11 @@ from django.utils import timezone
 from django.utils.timezone import now
 
 import VLE.permissions as permissions
+from VLE.utils import sanitization
 from VLE.utils.error_handling import (VLEParticipationError,
                                       VLEPermissionError, VLEProgrammingError,
                                       VLEUnverifiedEmailError)
 from VLE.utils.file_handling import get_path
-from VLE.utils import sanitization
 
 
 class Instance(models.Model):
