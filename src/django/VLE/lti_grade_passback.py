f6867c02098404bc949ca9ed06bd8dc3c28af8a6
Engel Hamer <39912581+engelhamer@users.noreply.github.com>
2018-11-28 11:14:04 +0100

    Fixed spelling error

--- src/django/VLE/lti_grade_passback.py
+++ src/django/VLE/lti_grade_passback.py
@@ -167,11 +167,11 @@ def needs_grading(journal, node):
 
     node_id = node.pk
     journal_id = journal.pk
-    aissignmnet_id = journal.assignment.pk
+    assignment_id = journal.assignment.pk
     course_id = journal.assignment.courses.order_by('-startdate').first().pk
 
     result_data = {'url': '{0}/Home/Course/{1}/Assignment/{2}/Journal/{3}?nID={4}'.format(
-        settings.BASELINK, course_id, aissignmnet_id, journal_id, node_id)}
+        settings.BASELINK, course_id, assignment_id, journal_id, node_id)}
     grade_request = GradePassBackRequest(key, secret, journal, result_data=result_data,
                                          submitted_at=str(node.entry.last_edited))
 
