b823c759ca170e8a56217a6a4f8cbc1ccf20c188
Lars van Hijfte <larsvanhijfte@hotmail.com>
2018-11-28 19:38:36 +0100

    Fixed assignments showing up as unpublished when they are not

--- src/vue/src/components/lti/LinkAssignment.vue
+++ src/vue/src/components/lti/LinkAssignment.vue
@@ -2,10 +2,10 @@
     <b-card class="no-hover">
         <div v-for="a in assignments" :key="a.id">
             <div v-if="a.lti_couples">
-                <assignment-card class="orange-border" @click.native="linkAssignment(a.id, a.lti_couples)" :line1="a.name"/>
+                <assignment-card class="orange-border" @click.native="linkAssignment(a.id, a.lti_couples)" :assignment="a"/>
             </div>
             <div v-else>
-                <assignment-card class="green-border" @click.native="linkAssignment(a.id, a.lti_couples)" :line1="a.name"/>
+                <assignment-card class="green-border" @click.native="linkAssignment(a.id, a.lti_couples)" :assignment="a"/>
             </div>
         </div>
     </b-card>
@@ -32,7 +32,7 @@ export default {
                 .then(assignments => { this.assignments = assignments })
         },
         linkAssignment (aID, aLtiCouples) {
-            if (!aLtiCouples || confirm('This assignment is already linked to ' + aLtiCouples + ' other assignment(s) from the learning-environment, are you sure you also want to link it?')) {
+            if (!aLtiCouples || confirm('This assignment is already linked to ' + (aLtiCouples > 1 ? aLtiCouples + ' ' : 'an') + 'other assignment' + (aLtiCouples > 1 ? 's' : '') + ' from the learning-environment, are you sure you also want to link it?')) {
                 assignmentAPI.update(aID, {lti_id: this.lti.ltiAssignID,
                     points_possible: this.lti.ltiPointsPossible,
                     is_published: this.lti.ltiAssignPublished,
