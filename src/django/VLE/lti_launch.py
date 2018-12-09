c741acf6d53680fb96cf2293c24c702ce66375d9
Lars van Hijfte <larsvanhijfte@hotmail.com>
2018-12-09 19:59:55 +0100

    requested changes

--- src/django/VLE/lti_launch.py
+++ src/django/VLE/lti_launch.py
@@ -99,7 +99,7 @@ def check_course_lti(request, user, role):
         return None
 
     course = lti_couple.course
-    lti_id, = utils.optional_params(request, 'custom_section_ids')
+    lti_id, = utils.optional_params(request, 'custom_section_id')
     # If the user is participatant, but not yet in a group, put the user in the Canvas related group.
     if user.is_participant(course):
         participation = Participation.objects.get(course=course, user=user)
