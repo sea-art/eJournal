7aeb597477486f94b729ec4d8ade14ceecaab7a0
Maarten van Keulen <mjvkeulen1@gmail.com>
2019-01-18 19:59:30 +0100

    CourseEdit filters moved to store.

--- src/vue/src/store/modules/preferences.js
+++ src/vue/src/store/modules/preferences.js
@@ -6,7 +6,15 @@ const getters = {
     journalSortAscending: state => state.journal.sortAscending,
     journalGroupFilter: state => state.journal.groupFilter,
     journalSearchValue: state => state.journal.searchValue,
-    journalSortBy: state => state.journal.sortBy
+    journalSortBy: state => state.journal.sortBy,
+    courseMembersSortAscending: state => state.courseMembers.sortAscending,
+    courseMembersViewEnrolled: state => state.courseMembers.viewEnrolled,
+    courseMembersGroupFilter: state => state.courseMembers.groupFilter,
+    courseMembersSearchValue: state => state.courseMembers.searchValue,
+    courseMembersSortBy: state => state.courseMembers.sortBy,
+    assignmentSortAscending: state => state.assignmentOverview.sortAscending,
+    assignmentSearchValue: state => state.assignmentOverview.searchValue,
+    assignmentOverviewSortBy: state => state.assignmentOverview.sortBy
 }
 
 const mutations = {
@@ -14,8 +22,8 @@ const mutations = {
         if (!preferenceOptions.TODO_SORT_OPTIONS.has(sortByOption)) { throw new Error('Invalid TODO sorting option.') }
         state.todo.sortBy = sortByOption
     },
-    [types.SET_JOURNAL_SORT_ASCENDING] (state, ascending) {
-        state.journal.sortAscending = ascending
+    [types.SET_JOURNAL_SORT_ASCENDING] (state, sortAscending) {
+        state.journal.sortAscending = sortAscending
     },
     [types.SET_JOURNAL_GROUP_FILTER] (state, groupFilter) {
         state.journal.groupFilter = groupFilter
@@ -26,6 +34,32 @@ const mutations = {
     [types.SET_JOURNAL_SORT_BY] (state, sortByOption) {
         if (!preferenceOptions.JOURNAL_SORT_OPTIONS.has(sortByOption)) { throw new Error('Invalid journal sorting option.') }
         state.journal.sortBy = sortByOption
+    },
+    [types.SET_COURSE_MEMBERS_SORT_ASCENDING] (state, sortAscending) {
+        state.courseMembers.sortAscending = sortAscending
+    },
+    [types.SET_COURSE_MEMBERS_VIEW_ENROLLED] (state, viewEnrolled) {
+        state.courseMembers.viewEnrolled = viewEnrolled
+    },
+    [types.SET_COURSE_MEMBERS_GROUP_FILTER] (state, groupFilter) {
+        state.courseMembers.groupFilter = groupFilter
+    },
+    [types.SET_COURSE_MEMBERS_SEARCH_VALUE] (state, searchValue) {
+        state.courseMembers.searchValue = searchValue
+    },
+    [types.SET_COURSE_MEMBERS_SORT_BY] (state, sortByOption) {
+        if (!preferenceOptions.COURSE_MEMBER_SORT_OPTIONS.has(sortByOption)) { throw new Error('Invalid course member sorting option.') }
+        state.courseMembers.sortBy = sortByOption
+    },
+    [types.SET_ASSIGNMENT_OVERVIEW_SORT_ASCENDING] (state, sortAscending) {
+        state.assignmentOverview.sortAscending = sortAscending
+    },
+    [types.SET_ASSIGNMENT_OVERVIEW_SEARCH_VALUE] (state, searchValue) {
+        state.assignmentOverview.searchValue = searchValue
+    },
+    [types.SET_ASSIGNMENT_OVERVIEW_SORT_BY] (state, sortByOption) {
+        if (!preferenceOptions.ASSIGNMENT_OVERVIEW_SORT_OPTIONS.has(sortByOption)) { throw new Error('Invalid assignment overview sorting option.') }
+        state.assignmentOverview.sortBy = sortByOption
     }
 }
 
@@ -43,14 +77,14 @@ export default {
         },
         courseMembers: {
             sortAscending: true,
-            enrolled: true,
+            viewEnrolled: true,
             groupFilter: null,
-            searchValue: null,
+            searchValue: '',
             sortBy: 'name'
         },
         assignmentOverview: {
             sortAscending: true,
-            searchValue: null,
+            searchValue: '',
             sortBy: 'name'
         }
     },
