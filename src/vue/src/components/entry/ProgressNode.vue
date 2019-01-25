3a12a84c8f5cb8da1cb5317c4feb293131fbeaaa
Engel Hamer <39912581+engelhamer@users.noreply.github.com>
2019-01-25 20:53:20 +0100

    Reversed comparison order and added spaces

--- src/vue/src/components/entry/ProgressNode.vue
+++ src/vue/src/components/entry/ProgressNode.vue
@@ -1,9 +1,9 @@
 <template>
     <b-card class="no-hover" :class="this.accomplished ? 'red-border' : 'green-border'">
         <h2 class="mb-2">Goal: {{ currentNode.target }} points</h2>
-        <span v-if="this.accomplished">
+        <span v-if="!this.accomplished">
             <b>{{ score }}</b> out of <b>{{ currentNode.target }}</b> points.<br/>
-            <b>{{ Math.round(left * 1000)/1000 }}</b> more required before <b>{{ $root.beautifyDate(currentNode.deadline) }}</b>.<br/>
+            <b>{{ Math.round(left * 1000) / 1000 }}</b> more required before <b>{{ $root.beautifyDate(currentNode.deadline) }}</b>.<br/>
         </span>
         <p>{{ currentNode.description }}</p>
     </b-card>
@@ -32,7 +32,7 @@ export default {
             return tempProgress
         },
         accomplished () {
-            return this.score < this.currentNode.target
+            return this.score > this.currentNode.target
         },
         left () {
             return this.currentNode.target - this.score
