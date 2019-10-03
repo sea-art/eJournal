fce449220939d424ce8654ef77f5928d91e68774
Lars <larsvanhijfte@hotmail.com>
2019-10-03 12:40:32 +0200

    Updated dependencies (#695)
    * Updated dependencies
    
    * Updated Django
    
    * isort
    
    * Updated dependencies once again

--- src/django/test/factory/__init__.py
+++ src/django/test/factory/__init__.py
@@ -1,16 +1,16 @@
 from test.factory.assignment import AssignmentFactory, LtiAssignmentFactory, TemplateAssignmentFactory
 from test.factory.comment import StudentCommentFactory, TeacherCommentFactory
 from test.factory.course import CourseFactory, LtiCourseFactory
-from test.factory.user import UserFactory, LtiStudentFactory, TeacherFactory, AdminFactory
-from test.factory.format import FormatFactory, TemplateFormatFactory
 from test.factory.entry import EntryFactory
+from test.factory.format import FormatFactory, TemplateFormatFactory
 from test.factory.grade import GradeFactory
 from test.factory.group import GroupFactory, LtiGroupFactory
 from test.factory.instance import InstanceFactory
 from test.factory.journal import JournalFactory
 from test.factory.participation import GroupParticipationFactory, ParticipationFactory
 from test.factory.role import RoleFactory, StudentRoleFactory
-from test.factory.template import TemplateFactory, TemplateAllTypesFactory
+from test.factory.template import TemplateAllTypesFactory, TemplateFactory
+from test.factory.user import AdminFactory, LtiStudentFactory, TeacherFactory, UserFactory
 
 Instance = InstanceFactory
 Assignment = AssignmentFactory
