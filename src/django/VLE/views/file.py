b0a86ffafeca6c3898a73a88e957fc5b4d18d361
M.J. van Keulen <mjvkeulen1@gmail.com>
2019-12-18 11:37:02 +0100

    Allow any file download

--- src/django/VLE/views/file.py
+++ src/django/VLE/views/file.py
@@ -3,6 +3,8 @@ from rest_framework import viewsets
 import VLE.utils.responses as response
 import VLE.validators as validators
 from VLE.models import FileContext
+from rest_framework.permissions import AllowAny
+import re
 
 
 class FileView(viewsets.ViewSet):
@@ -29,3 +31,9 @@ class FileView(viewsets.ViewSet):
             description='Successfully uploaded {:s}.'.format(request.FILES['file'].name),
             payload={'download_url': file.download_url()}
         )
+
+    def get_permissions(self):
+        if re.search(r'^/files/\d+/', self.request.path) and self.request.method == 'GET':
+            return [AllowAny()]
+        else:
+            return [permission() for permission in self.permission_classes]
