6bbb4a3d921444372026b65b7ca2f94baf2a2635
Engel Hamer <engelhamer@gmail.com>
2020-01-19 21:57:16 +0100

    0 also said point

--- src/vue/src/components/assets/ProgressBar.vue
+++ src/vue/src/components/assets/ProgressBar.vue
@@ -7,8 +7,8 @@
             <b>{{ currentPoints ? currentPoints : 0 }}</b> Points
             <tooltip
                 v-if="bonusPoints != 0"
-                :tip="`${currentPoints - bonusPoints} journal point${Math.abs(currentPoints - bonusPoints) > 1 ? 's' :
-                    ''} + ${bonusPoints} bonus point${Math.abs(bonusPoints) > 1 ? 's' : ''}`"
+                :tip="`${currentPoints - bonusPoints} journal point${Math.abs(currentPoints - bonusPoints) === 1 ? 's'
+                    : ''} + ${bonusPoints} bonus point${Math.abs(bonusPoints) > 1 ? 's' : ''}`"
             />
         </h5>
         <b-progress
@@ -48,7 +48,7 @@
                 name="star"
                 class="fill-orange shift-up-2 mr-1"
             />
-            <b>{{ bonusPoints }}</b> bonus point{{ Math.abs(bonusPoints) > 1 ? "s" : "" }}
+            <b>{{ bonusPoints }}</b> bonus point{{ Math.abs(bonusPoints) === 1 ? "s" : "" }}
             <br/>
         </span>
         <span v-if="comparePoints >= 0">
