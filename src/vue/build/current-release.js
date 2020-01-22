33fc480c7158f34bbc02e99ed102d69ff008da47
Lars van Hijfte <larsvanhijfte@hotmail.com>
2020-01-22 21:47:47 +0100

    Fixed git tag bug

--- src/vue/build/current-release.js
+++ src/vue/build/current-release.js
@@ -1,6 +1,5 @@
 const execFileSync = require('child_process').execFileSync
-
-const version = execFileSync('git', ['describe', '--abbrev=0', '--tags']).toString().trim()
+const version = execFileSync('git', ['for-each-ref', '--format="%(refname:short)"', '--sort=-authordate', '--count=1', 'refs/tags']).toString().trim()
 const rawMessage = execFileSync('git', ['tag', '-l', '-n3', version]).toString().split(/\n\s*\n/)
 const date = new Date(execFileSync('git', ['tag', '-l', '-n1', '--format=\'%(taggerdate)\'', version]).toString()
     .trim())
