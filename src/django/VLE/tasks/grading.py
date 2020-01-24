2c8dbc6166de88025d399c4b0a98c8048a3fec7d
Lars van Hijfte <larsvanhijfte@hotmail.com>
2020-01-24 15:14:54 +0100

    cleanup

--- src/django/VLE/tasks/grading.py
+++
@@ -1,7 +0,0 @@
-from __future__ import absolute_import, unicode_literals
-
-from celery import shared_task
-
-from VLE import factory
-from VLE.models import AssignmentParticipation, Comment, Entry, Journal
-from VLE.utils import grading
