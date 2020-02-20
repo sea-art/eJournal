1b137f64274a082abd83fe06d159784a66839e6b
Engel Hamer <engelhamer@gmail.com>
2020-02-20 16:59:17 +0100

    Package number should be according to semver

--- src/vue/src/components/assets/Footer.vue
+++ src/vue/src/components/assets/Footer.vue
@@ -1,26 +1,29 @@
 <template>
     <footer>
         <hr/>
-        &copy; 2020 - eJournal.app -
+        &copy; 2020 - eJournal (
+        <a
+            href="https://www.eJournal.app/changelog"
+            class="text-grey underline"
+        >
+            {{ version }}
+        </a>
+        ) -
         <a
             href="/eJournal-privacy-notice.pdf"
-            class="text-grey"
+            class="text-grey underline"
         >
             Privacy notice
         </a>
-        <tooltip
-            tip="We reserve the right to change this privacy policy as we may deem necessary from time to time or as
-            may be required by law. Any changes will be immediately posted on the website and you are deemed to have
-            accepted the terms of the privacy policy on your first use of the website following the alterations."
-        />
     </footer>
 </template>
-<script>
-import tooltip from '@/components/assets/Tooltip.vue'
 
+<script>
 export default {
-    components: {
-        tooltip,
+    data () {
+        return {
+            version: CurrentRelease.version,
+        }
     },
 }
 
@@ -32,4 +35,6 @@ footer
     text-align: center
     width: 100%
     margin: 10px 0px
+    a.underline
+        text-decoration: underline !important
 </style>
--- src/vue/src/components/assets/Tooltip.vue
+++ src/vue/src/components/assets/Tooltip.vue
@@ -1,12 +1,10 @@
 <template>
-    <sup>
-        <icon
-            v-b-tooltip.hover
-            :title="tip"
-            name="info"
-            class="info-icon no-hover help-cursor"
-        />
-    </sup>
+    <icon
+        v-b-tooltip.hover
+        :title="tip"
+        name="info"
+        class="info-icon no-hover help-cursor shift-up-3"
+    />
 </template>
 <script>
 
