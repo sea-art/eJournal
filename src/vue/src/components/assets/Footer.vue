053758d10f2fe3bb77c63f8e396952680060f83b
Engel Hamer <engelhamer@gmail.com>
2020-03-04 17:49:25 +0100

    Bumped version number

--- src/vue/src/components/assets/Footer.vue
+++ src/vue/src/components/assets/Footer.vue
@@ -1,13 +1,12 @@
 <template>
     <footer>
-        <hr/>
-        &copy; 2020 - eJournal (
+        &copy; 2020 - eJournal
         <a
             href="https://www.eJournal.app/changelog"
             class="text-grey"
         >
             {{ version }}</a>
-        ) -
+        -
         <a
             href="mailto:support@eJournal.app"
             class="text-grey"
@@ -35,7 +34,14 @@ export default {
 </script>
 
 <style lang="sass">
+@import '~sass/modules/colors.sass'
+
 footer
+    border-width: 2px 2px 2px 2px
+    border-color: $theme-dark-grey
+    border-radius: 1em !important
+    border-left: 8px solid $theme-blue
+    border: 1px solid $theme-dark-grey
     color: grey
     text-align: center
     width: 100%
